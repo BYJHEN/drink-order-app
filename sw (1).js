e13b00e4c17c0aec8877d4d6ed5ddb7554aaa6c4
BYJHEN <youjhen1207@gmail.com>
2026-07-15 13:31:56 +0800

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,17 @@
+{
+  "name": "一起點餐",
+  "short_name": "一起點餐",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#f5f3ed",
+  "theme_color": "#756e5f",
+  "lang": "zh-TW",
+  "icons": [
+    {
+      "src": "app-icon.png",
+      "sizes": "any",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
